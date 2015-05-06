54abe213516fcfd30a3ed2e91650a5855ba6c61c
pipyui <alex.nemeth9@gmail.com>
2015-05-06 17:13:54 -0400

    Update ncurses_wrapper.hpp

--- ncurses_wrapper.hpp
+++ ncurses_wrapper.hpp
@@ -6,14 +6,20 @@
 #include <ncurses.h>
 
 
-constexpr short SIDEBAR_WIDTH = 20;
+constexpr short SIDEBAR_WIDTH = 15;
 
 WINDOW* create_window(int height, int width, int starty, int startx);
+int get_color(color_block_t);
+void instantialize_color_pairs();
 
 class ncurses_wrapper {
 private:
 	WINDOW *field;
 	WINDOW *bar;
+
+	void draw_settled(playField &);
+	void draw_currentBlock(playField &);
+	void draw_preview(sidebar &);
 
 public:
 	ncurses_wrapper();
