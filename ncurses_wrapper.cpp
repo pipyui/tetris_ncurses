bf6a53322efb4e06276ccc7351a8b3af6a01e211
pipyui <alex.nemeth9@gmail.com>
2015-05-06 17:14:17 -0400

    Update ncurses_wrapper.cpp

--- ncurses_wrapper.cpp
+++ ncurses_wrapper.cpp
@@ -48,33 +48,59 @@ void instantialize_color_pairs() {
 }
 
 ncurses_wrapper::ncurses_wrapper()
- : field(create_window(WELL_HEIGHT, WELL_WIDTH, 1, 0)),
-   bar(create_window(WELL_HEIGHT, SIDEBAR_WIDTH, 1, WELL_WIDTH + 1)) {};
+ : field(create_window(WELL_HEIGHT, WELL_WIDTH, 1, SIDEBAR_WIDTH + 1)),
+   bar(create_window(WELL_HEIGHT, SIDEBAR_WIDTH, 1, 0)) {};
 
 ncurses_wrapper::~ncurses_wrapper() {
 	delwin(field);
 	delwin(bar);
 };
 
+void ncurses_wrapper::draw_settled(playField &pf) {
+	for (short i = 0; i < WELL_HEIGHT - 1; ++i)
+		for (short j = 1; j < WELL_WIDTH - 1; ++j)
+			if (pf.gameGrid[i][j] != EMPTY)
+				mvwaddch( field, i, j, '#' | COLOR_PAIR(get_color(pf.gameGrid[i][j])) );
+}
+
+void ncurses_wrapper::draw_currentBlock(playField &pf) {
+	grid_box blocks = pf.currentBlock.get_blocks();
+	for (short i = 0; i < BLOCK_SIZE; ++i)
+		for (short j = 0; j < BLOCK_SIZE; ++j)
+			if (blocks.grid[i][j] != EMPTY)
+				mvwaddch( bar, pf.currentX + i, pf.currentY + j, '#' | COLOR_PAIR(get_color(blocks.grid[i][j])) );
+}
+
+void ncurses_wrapper::draw_preview(sidebar &sb) {
+	iBlock next = sb.get_nextBlock();
+	grid_box preview = next.get_blocks();
+	for (short i = 0; i < BLOCK_SIZE; ++i)
+		for (short j = 0; j < BLOCK_SIZE; ++j)
+			if (preview.grid[i][j] != EMPTY)
+				mvwaddch( bar, i + 3, j + SIDEBAR_WIDTH / 2 - 2, '#' | COLOR_PAIR(get_color(preview.grid[i][j])) );
+}
+
 void ncurses_wrapper::draw_playField(playField &pf) {
 	werase(field);
 	box(field, 0, 0);
+
+	draw_settled(pf);
+	draw_currentBlock(pf);
+
 	wrefresh(field);
 }
 
 void ncurses_wrapper::draw_sidebar(sidebar &sb) {
 	werase(bar);
 	box(bar, 0, 0);
-	iBlock next = sb.get_nextBlock();
-	grid_box preview = next.get_blocks();
 	int score = sb.get_score();
 
-	for (short i = 0; i < BLOCK_SIZE; ++i) {
-		for (short j = 0; j < BLOCK_SIZE; ++j) {
-			if (preview.grid[i][j] != EMPTY)
-				mvwaddch( bar, i + 1, j + 1, '#' | COLOR_PAIR(get_color(preview.grid[i][j])) );
-		}
-	}
-	mvwprintw( bar, WELL_HEIGHT - 3, 5, "Score: %d", score);
+	mvwprintw( bar, 1, SIDEBAR_WIDTH / 2 - 5, "Next Block:");
+	mvwprintw( bar, WELL_HEIGHT - 8, 1, "F1 = New Game");
+	mvwprintw( bar, WELL_HEIGHT - 6, 1, "F3 = Quit");
+	
+	draw_preview(sb);
+
+	mvwprintw( bar, WELL_HEIGHT - 3, 1, "Score: %d", score);
 	wrefresh(bar);
 }
